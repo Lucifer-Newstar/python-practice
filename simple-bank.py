0b676f3adcfa2262316e7a8b385e73cb46e7212d
Navin Jairam M <navin.jairam@gmail.com>
2026-05-18 00:16:12 +0530

    a simple bank program that withdraws and shows balance with while and for loops I'm so peak that I made it working in first commit

---
+++ simple-bank.py
@@ -0,0 +1,39 @@
+#just so I should learn how to give documentation you need to read this
+#this is just a simple bank program using simple looping statements 
+#yes code logic might be dumb but I'm cool 
+
+
+#get acc no and assign amount
+acc_no = input("Enter your account number:")
+#just because this is a sample program you can't expect me to let you give random ahh bullshi in the account number(ig you could still do)
+while len(acc_no)<14 or len(acc_no)>14: # I could use len(acc_no) != 14 but I won't
+    print ("Account number is wrong")
+    acc_no = input("Enter your account number:")
+#considering you are NOT broke enough yet
+initial_amount = 500
+
+#assign actions
+act = input("Welcome to the bank choose your action: 1. Withdrawal, 2. Check Balance, 3. Exit:")
+
+#using while function where exit isn't chosen. code logic is shit ik bear with it I'm js a kid ``uwu``
+while not act == "3":
+    if act == "2":
+        print(f"The balance amount in your account XXXX-XXXX-{acc_no[-4:]} is : $ {initial_amount} ")
+        print("Thank you for using our bank!")
+        act = input("Welcome to the bank choose your action: 1. Withdrawal, 2. Check Balance, 3. Exit:")
+    elif act == "1":
+        wamt = int(input("Enter the amount to be withdrawn:"))
+        if wamt > initial_amount:
+            print(f"Insufficient bank balance! Remaining balance: {initial_amount}")
+            act = input("Welcome to the bank choose your action: 1. Withdrawal, 2. Check Balance, 3. Exit:")
+        else:
+            ramt = initial_amount - wamt
+            initial_amount = ramt
+            print(f"The amount {wamt} is withdrawn. The remaining balance is : {initial_amount}")
+            act = input("Do you wish to continue? Choose your action: 1. Withdrawal, 2. Check Balance, 3. Exit:")
+    else:
+        print("Choose from the actions")
+        act = input("Welcome to the bank choose your action: 1. Withdrawal, 2. Check Balance, 3. Exit:")
+
+
+print ("Thank you!")
